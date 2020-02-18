bab8b7901f86ae429a652104f2f63476099796a6
Teck Mee_May <kongteckmee@gmail.com>
2020-02-18 16:29:41 +0000

    Control map using drop down

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,19 @@
+// Display Information Modal
 $(document).ready(function() {
     $("#myCenterModal").modal('show');
 });
+
+// Location of Map
+    var MapZoom = 0;
+    $("#countries").on("change", function() {
+    var country = $(this).val().split(","),
+        MapLat = country[0],
+        MapLng = country[1],
+        MapZoom = parseInt(country[2]),
+        location = [MapLat, MapLng]
+});
+
+var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: MapZoom, center: location});
+
+var marker = new google.maps.Marker({position: location, map: map});
